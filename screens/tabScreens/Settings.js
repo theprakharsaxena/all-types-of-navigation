2435370fbe1b400c7114bc0aa9e208a52c016f8d
Prakhar Saxena <prakharsaxena5125@gmail.com>
2024-04-07 18:10:11 +0530

    update

--- screens/tabScreens/Settings.js
+++ screens/tabScreens/Settings.js
@@ -1,6 +1,8 @@
 import { Button, SafeAreaView, StyleSheet, Text } from "react-native";
+import { useNavigation } from "@react-navigation/native";
 
 const Settings = () => {
+  const navigation = useNavigation();
   return (
     <SafeAreaView>
       <Text>Payments</Text>
